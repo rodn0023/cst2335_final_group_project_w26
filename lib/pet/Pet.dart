2e05d34aaf2451163c52a80d7d936da8fb6927d4
Dirgh <pate1641@alginquinlive.com>
2026-04-10 20:01:31 -0400

    completion of pet file

--- lib/pet/Pet.dart
+++ lib/pet/Pet.dart
@@ -1,1 +1,22 @@
 //Dirgh
+/// Simple Pet class for Step 1.
+/// This version is only for the basic list page before Floor database is added.
+class Pet {
+  /// Pet name.
+  String name;
+
+  /// Pet birthday.
+  String birthday;
+
+  /// Pet species.
+  String species;
+
+  /// Pet colour.
+  String colour;
+
+  /// Owner id.
+  String ownerId;
+
+  /// Creates one Pet object.
+  Pet(this.name, this.birthday, this.species, this.colour, this.ownerId);
+}
